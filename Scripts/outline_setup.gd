54ec3f1f7a82a1dda64e317fadde0161d3defaef
steincodes <swarnimarun11@gmail.com>
2018-04-30 21:37:27 +0530

    Updating debug statements

--- Scripts/outline_setup.gd
+++ Scripts/outline_setup.gd
@@ -2,7 +2,7 @@ extends Sprite
 
 func _ready():
 	var size = texture.get_data().get_size()
-	print(size)
+	#print(size)
 	self.material.set_shader_param("sizex", size.x)
 	self.material.set_shader_param("sizey", size.y)
 
